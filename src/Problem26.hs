2b1c258e1c901d415864b57e430132bad99c055e
Alex Smith <alex.smith@redhat.com>
2014-11-28 22:34:15 -0500

    Move prime checking functions into separate file.

--- src/Problem26.hs
+++ src/Problem26.hs
@@ -24,18 +24,3 @@ findLongestCycle = (x, n, (one / (fromIntegral x)))
       if n1 > n2
       then (x1, n1)
       else (x2, n2)
-
-divisors :: Integer -> [Integer]
-divisors n
-  | n == 1 = [1]
-  | n > 0 = 1:(xs ++ ys)
-  | otherwise = error "Value must be greater than zero."
-  where
-    isqrt = floor . sqrt . fromIntegral
-    xs = [x | x<-[2 .. isqrt n], n `mod` x == 0]
-    ys = [n `div` x | x<-xs, x /= n `div` x]
-
-isPrime 1 = False
-isPrime n = divisors n == [1]
-
-primeDivisors n = filter isPrime (divisors n)
