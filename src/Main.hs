5db459105a221436f2318232b541ba5068c81bf6
Alex Smith <alex.smith@redhat.com>
2015-09-12 21:17:04 -0400

    Add solution for problem 3.

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 import System.Environment (getArgs)
 import qualified Problem1
 import qualified Problem2
+import qualified Problem3
 import qualified Problem11
 import qualified Problem15
 import qualified Problem26
@@ -14,6 +15,7 @@ main = do
 
     solve 1  = Problem1.solve
     solve 2  = Problem2.solve
+    solve 3  = Problem3.solve
     solve 11 = Problem11.solve
     solve 15 = Problem15.solve
     solve 26 = Problem26.solve
