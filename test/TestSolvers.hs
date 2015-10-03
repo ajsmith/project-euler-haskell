537bc5495502ffdfc6d09b5225c3d007745cbd30
Alex Smith <alex.smith@redhat.com>
2015-10-03 11:43:44 -0400

    Add test for problem 15.

--- test/TestSolvers.hs
+++ test/TestSolvers.hs
@@ -1,6 +1,7 @@
 module TestSolvers where
 import Distribution.TestSuite
 import qualified Problem1
+import qualified Problem15
 import qualified Problem37
 
 
@@ -19,7 +20,8 @@ tests = return tests'
           , setOption = \_ _ -> Left problem
           }
 
-    tc1 = testCase "Problem1" (Problem1.solve 0 == 233168)
+    tc1  = testCase "Problem1"  (Problem1.solve  0 == 233168)
+    tc15 = testCase "Problem15" (Problem15.solve 0 == 137846528820)
     tc37 = testCase "Problem37" (Problem37.solve 0 == 748317)
 
     check bool =
