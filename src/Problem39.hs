00a4cd620a773b6237fa9af230ce4dfe376405c6
Alex Smith <alex.smith@redhat.com>
2016-03-27 01:44:08 -0400

    Remove commented out code.

--- src/Problem39.hs
+++ src/Problem39.hs
@@ -14,25 +14,6 @@ import Data.List (groupBy, nubBy)
 import Data.Set (empty, insert, toAscList)
 import PythagoreanTriples (triplesForPerimeter)
 
--- trianglesFor :: Integral a => a -> [[a]]
--- trianglesFor p = nubBy (\ls1 ls2 -> head ls1 == head ls2) sides
---   where
---     sides = [[c, a, b] | a<-ls, b<-ls, c<-ls, a + b + c == p, a^2 + b^2 == c^2]
---     ls = [1..p]
-
--- triangles :: Integral a => [[a]]
--- triangles = [tri a b c | a<-ls, b<-ls, c<-ls, a + b + c <= limit, a^2 + b^2 == c^2]
---   where
---     limit = 1000
---     ls = [1..(limit-2)]
---     tri a b c
---       | a < b     = [a, b, c]
---       | otherwise = [b, a, c]
-
--- trianglesByPerimeter = groupBy groupByPerimeter $ toAscList $ foldr insert empty triangles
---   where
---     groupByPerimeter ts1 ts2 = sum ts1 == sum ts2
-
 maxTriangles ts1 ts2 =
   if length ts1 > length ts2
   then ts1
